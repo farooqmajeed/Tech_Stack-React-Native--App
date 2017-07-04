bfafd9e68ef731188a95ce851ad05673c0ac84a5
farooqmajeed <farooq306@hotmail.com>
2017-07-04 17:36:02 +0500

    tech-stack app using Redux

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -29,17 +29,7 @@ class ListItem extends Component{
         const {titleStyles} = styles;
         const {id, title} = this.props.library
         return (
-            <TouchableWithoutFeedback onPress={()=>this.props.selectLibrary(id)}>
-                <View>
-                    <CardSection>
-                        <Text style={titleStyles}>
-                            {title}
-                        </Text>
-                    </CardSection> 
-                    {this.renderDescription()}
-                </View>
-                
-            </TouchableWithoutFeedback>
+            
             
         );
     }
